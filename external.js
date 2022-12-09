b5c9fd5bb4df84275302f5c145f31a1b90f6def0
Syed Ali Abbas <aliabbasshah72@gmail.com>
2022-12-09 20:16:11 +0500

    Plays a Single Round

--- external.js
+++ external.js
@@ -1,7 +1,48 @@
+let arr = ["rock", "paper", "scissors"];
+let computerSelection = getComputerChoice();
+
 function getComputerChoice(){
-    let arr = ["Rock", "Paper", "Scissors"];
     let arr2 = arr[Math.floor(Math.random() * arr.length)];
     return arr2;
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === "rock" && computerSelection === "paper"){
+        return "You Lose! Paper Beats Rock";
+    } else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return "You Win! Scissors Beats Paper";
+    } else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return "You Win! Rock Beats Scissors";
+    } else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "You Win! Paper Beats Rock";
+    } else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return "You Lose! Scissors Beats Paper";
+    } else if(playerSelection === "scissors" && computerSelection === "rock"){
+        return "You Lose! Rock Beats Scissors";
+    } else if(playerSelection === computerSelection) {
+        return "Match Draw! Try Again";
+    } else {
+        return "ERROR 404";
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
+
+function game(){
+    let playerSelection = prompt("Enter Yours Choice").toLowerCase();
+    
+    for (let i = 0; i < 5; i++){
+        return playRound(playerSelection, computerSelection);
+    }
+    if(playerScore > computerScore){
+        return "WINNER";
+    } else if(playerScore === computerScore){
+        return "DRAW";
+    } else {
+        return "LOSER";
+    }
+}
+
+game();
